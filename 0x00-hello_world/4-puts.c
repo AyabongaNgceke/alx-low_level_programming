10003c13cf7b4346770808d6e7f643a5de0587ee
root <ngcekelelisa9@gmail.com>
2022-09-08 13:32:43 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+*main - print the string in put funtion
+*Description: using the main function
+*this program prints "Pogramming is like building a multilingual puzzle"
+Return: 0
+*/
+int main(void);
+{
+puts("\"Pogramming is like building a multilingual puzzle");
